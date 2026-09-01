ebfc880b68a0633dc4a99f3fcc6f467508a7e18c
abdelwahab-chaachoui <abdelwahabchaachoui@gmail.com>
2026-09-01 21:58:10 +0200

    cleanup

--- portfolio/blogs/api_urls.py
+++ portfolio/blogs/api_urls.py
@@ -4,7 +4,6 @@ from .viewset import PostViewSet, KeywordViewSet  # , CommentForPostViewSet
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
-#router.register(r'comments', CommentViewSet)
 
 router.register(r'keywords', KeywordViewSet)
 
