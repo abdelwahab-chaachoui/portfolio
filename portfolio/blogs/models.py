6810f66151020e86a25ad9cba009b04b372ca66d
abdelwahab-chaachoui <abdelwahabchaachoui@gmail.com>
2026-09-01 21:29:23 +0200

    cleanup

--- portfolio/blogs/models.py
+++ portfolio/blogs/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.db.models.fields.related import ForeignKey
 import uuid
-
-from django.utils import choices
 from django.utils.safestring import mark_safe
 
 STATUS = (
