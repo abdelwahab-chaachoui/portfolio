22de13abd2713eacbf36cbed3534a31ee778198d
abdelwahab-chaachoui <abdelwahabchaachoui@gmail.com>
2026-09-03 21:16:58 +0200

    init core template

--- portfolio/app/urls.py
+++ portfolio/app/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls')),
-    path('api/', include("blogs.api_urls"))
+    path('api/', include("blogs.api_urls")),
+    path('', include("core.urls"))
 ]
